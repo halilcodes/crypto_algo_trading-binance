a2263bb8b44585dac8e18853fa989b0ff13126fc
Halil_Hasmer <halilpython@gmail.com>
2023-09-17 13:10:25 +0300

    load historical data

--- Part1_Materials/hch_script.py
+++ Part1_Materials/hch_script.py
@@ -1,4 +1,11 @@
 import os
+from keys import testnet_api_public, testnet_api_secret
+from binance import Client, ThreadedWebsocketManager, ThreadedDepthCacheManager
 
 public_key = os.getenv("binance_udemy_public_key")
 secret_key = os.getenv("binance_udemy_secret_key")
+
+print(public_key)
+print(secret_key)
+
+client = Client(api_key=public_key, api_secret=secret_key, tld="com")
